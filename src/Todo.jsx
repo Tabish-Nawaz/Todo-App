cab678721e2d01311d49ea6c0b4b1b583d447e6d
Tabish-Nawaz <tabishnawaz311@gmail.com>
2026-03-31 01:35:28 +0500

    My First Todo App

---
+++ src/Todo.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+
+function Todo() {
+    const [newTodo, setNewTodo] = useState("")
+    const [todos, setTodos] = useState([])
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(newTodo) {
+            setTodos([...todos, {text: newTodo, completed: false }])
+            setNewTodo("");
+        }
+    }
+    const handleDelete = (index) => {
+        const newTodos = [...todos];
+        newTodos[index].completed = !newTodos[index].completed 
+        setTodos(newTodos)
+    }
+
+    return (
+    <>
+        <h1>Todo App</h1>
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder='Add New Todo' value={newTodo} onChange={(e) => {setNewTodo(e.target.value)}} />
+            <button onClick={handleDelete}>Add Todo</button>
+        </form>
+        <ul>
+                {todos.map((todo, index)=> (
+                    <li key={index}>
+                        <span style={{textDecoration: todo.completed ? "line-through": "none"}}>{todo.text}</span>
+                        <button onClick={() => handleDelete(index)}>Delete</button>
+                    </li>                    
+                ))}
+        </ul>
+    </>
+  )
+}
+
+export default Todo
